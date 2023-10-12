7a37860b6d2a2c3edffdcb0618d2da2532d79846
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-10-12 17:30:55 -0300

    Correções no sql e mapa botado no card

--- mapas/asynchronous/receive.php
+++ mapas/asynchronous/receive.php
@@ -1,11 +1,10 @@
 <?php
 require 'connection.php';
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $lat = filter_input(INPUT_GET, 'lat', FILTER_SANITIZE_SPECIAL_CHARS);
 $lgn = filter_input(INPUT_GET, 'lgn', FILTER_SANITIZE_SPECIAL_CHARS);
 
 if ($id && $lat && $lgn){
-    $conexao -> query("INSERT INTO device (id, lat, lgn) VALUES ('$id', '$lat', '$lgn')");
+    $conexao -> query("INSERT INTO device (lat, lgn) VALUES ($lat, $lgn)");
 }
 
 #redireciona
