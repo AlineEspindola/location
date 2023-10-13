fba5ee40b419a5db448b9467711053475e98cce8
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2023-10-13 17:01:48 -0300

    Pegar ID da última pessoa que entrar

--- mapas/asynchronous/receive.php
+++ mapas/asynchronous/receive.php
@@ -5,8 +5,7 @@ $lat = filter_input(INPUT_GET, 'lat', FILTER_SANITIZE_SPECIAL_CHARS);
 $lng = filter_input(INPUT_GET, 'lgn', FILTER_SANITIZE_SPECIAL_CHARS);
 
 
-//Transforma a String em Número Decimal
-//Indo por número direto, não pega o .
+//Transforma a String em Número Decimal. Indo por número direto, não pega o .   Ex: 28.9 vira 289
 $lat = floatval($lat);
 $lng = floatval($lng);
 
@@ -14,6 +13,10 @@ if ($lat && $lng){
     $connection -> query("INSERT INTO device (lat, lng) VALUES ($lat, $lng)");
 }
 
-header('Location: asynchronous.html');
+$id = $connection->query("SELECT id FROM device ORDER BY id DESC LIMIT 1");
+$data = $id->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($data);
+
+//header('Location: asynchronous.html');
 
 ?>
