0f4837208a3ba337065ae225375de5d440dd7368
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-10-13 21:27:11 -0300

    Mais organização do código

--- maps/asynchronous/display.php
+++ maps/asynchronous/display.php
@@ -6,7 +6,7 @@ $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $sql = "SELECT * FROM device WHERE $id=id ORDER BY id DESC LIMIT 1";
 $coordinates = $connection->query($sql);
-//Para transformar em formato json
+
 $data = $coordinates->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($data);
 
