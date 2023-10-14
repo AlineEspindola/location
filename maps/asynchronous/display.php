55c00e65ea823d53423421c82e8804d1ea398b5d
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2023-10-13 22:07:51 -0300

    Comentarios

--- maps/asynchronous/display.php
+++ maps/asynchronous/display.php
@@ -6,8 +6,10 @@ $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $sql = "SELECT * FROM device WHERE $id=id ORDER BY id DESC LIMIT 1";
 $coordinates = $connection->query($sql);
-//Para transformar em formato json
+
+//Transforma em array associativo, em que cada valor é acompanhado do que ele é. Ex: ao invés de 9, 45.8, 49.0 teremos id:9, lat:49.8, lng:49.0
 $data = $coordinates->fetchAll(PDO::FETCH_ASSOC);
+//Para transformar em formaro json. A resposta recebbia no asynchronous é o valor que é printado, no caso, o valor embaixo
 echo json_encode($data);
 
 ?>
